d2d66ccb4fe5806a5942121f2c5a7dbfbd435478
drussok <drussok@softprojekt.pl>
2024-06-29 20:46:02 +0200

    User module init

---
+++ web/src/main/java/damian/russok/web/user/UserController.java
@@ -0,0 +1,19 @@
+package damian.russok.web.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+}
